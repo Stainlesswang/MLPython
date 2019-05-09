ced24237076437813165b78a6d00715aa413adc6
allen <allenwangook@gmail.com>
2019-05-09 10:52:01 +0800

    learn c language

--- Clanguage/CPractice.c
+++ Clanguage/CPractice.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+
+#define SOME_THING 100
+#define PI (1.0f/3.1415926f)
  
+ /* Adds two fractions*/
 int main()
 {
-    // 基本数据类型在C语言中没什么特殊的兄弟
-    int a,b,c;
-    float profit;
-    printf("To c or not To that is aquestion!\n");
- 
- // #c语言当中的基本数据类型是什么   
+    float i;
+
+    i=72.99f;
+
+    printf("%f\n",i++ );
+    int num1,denom1,num2,denom2,result_num,result_denom;
+
+    printf("Enter first feaction:");
+    scanf("%d/%d",&num1,&denom1);
+
+    printf("Enter second feaction:");
+    scanf("%d/%d",&num2,&denom2);
+
+    result_num= num1 * denom1 + num2 * denom2;
+    result_denom = denom1 * denom2;
+
+    printf("the sum is %d/%d\n",result_num,result_denom );
+
+    return 0;
 }
