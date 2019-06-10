cbe9fe049ea3cf336a53a446ac50c1a50d3d74f3
allen <allenwangook@gmail.com>
2019-06-10 17:58:08 +0800

    add prime.c

---
+++ Clanguage/prime.c
@@ -0,0 +1,36 @@
+#include "stdbool.h"
+#include "stdio.h"
+
+
+bool is_prime(int n){
+	int divisor;
+	if (n<=1)
+		return false;
+	for (divisor = 2; divisor * divisor <= n; divisor++)
+	{
+		if (n%divisor==0)
+		{
+			/* code */
+			return false;
+		}
+		/* code */
+	}
+	return true;
+}
+
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	printf("Enter a number: ");
+	scanf("%d",&n);
+	if (is_prime(n))
+	{
+		/* code */
+		printf("Prime \n");
+	}else{
+		printf("Not Prime \n");
+	}
+	
+	return 0;
+}
--- Clanguage/repdigit.c
+++ Clanguage/repdigit.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int  main(int argc, char const *argv[])
-{
-    for (int i = 0; i < argc; ++i) {
-        printf("%d\n",argc );
-        printf("%s\n",argv[i] );
-    }
-	return 0;
+int main(){//变量命名规则,只能使用数字字母下划线.  并且只能以字母下划线开头
+	// now i will give 首先看看家里有啥习题类的书籍. 然后买一本
+	bool digit_seen[10] = {false};
+	int digit;
+	long n;
+	int 34wo=3;
+	printf("%d\n",34wo );
+
 }
